949903d3b2f08e0f9e32aeb7093fb27576089373
Ugurhamzaoglu67 <craxx67@gmail.com>
2022-04-10 11:03:59 +0300

    Custom SearchBar and Title added

--- GithubJobs-APP/Views/MainUICollectionVC.swift
+++ GithubJobs-APP/Views/MainUICollectionVC.swift
@@ -16,17 +16,21 @@ class MainUICollectionVC: UICollectionViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
+        searchBarConfigure()
+    }
+    
+    private func setupViews() {
         collectionView.delegate = self
         collectionView.dataSource = self
         collectionView.backgroundColor = .lightGray
         collectionView.register(HomeCollectionCell.self,
                                 forCellWithReuseIdentifier: HomeCollectionCell.identifier)
-        searchBarConfigure()
     }
     
     
-    
     private func  searchBarConfigure() {
+        customSearchBarStyle()
         navigationItem.hidesSearchBarWhenScrolling = false
         navigationItem.searchController = self.searchController
         searchController.searchBar.tintColor  = .black
@@ -36,6 +40,26 @@ class MainUICollectionVC: UICollectionViewController{
         searchController.searchBar.searchTextField.defaultTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.black]
         searchController.searchBar.barTintColor = .black
        
+    }
+    
+    private func customSearchBarStyle(){
+        
+         if #available(iOS 13.0, *) {
+            let navBarAppearance = UINavigationBarAppearance()
+            //navBarAppearance.configureWithOpaqueBackground()
+
+            navBarAppearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white,NSAttributedString.Key.font: UIFont(name: "Chalkduster", size: 28.0)!]
+            navBarAppearance.backgroundColor = .purple
+            navigationController?.navigationBar.barStyle = .black
+            navigationController?.navigationBar.standardAppearance = navBarAppearance
+            navigationController?.navigationBar.compactAppearance = navBarAppearance
+            navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+
+            navigationController?.navigationBar.prefersLargeTitles = false
+            navigationItem.title = "Job Finder®"
+
+            }
+         
     }
     
    
