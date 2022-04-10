a51525b291ae4064645990ed6205d68d11c73814
Ugurhamzaoglu67 <craxx67@gmail.com>
2022-04-10 05:43:56 +0300

    first commit

---
+++ GithubJobs-APP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubJobs-APP
+//
+//  Created by ugur-pc on 10.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
