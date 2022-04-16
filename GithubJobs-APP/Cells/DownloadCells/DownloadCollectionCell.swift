11bf062c368a3c07325c082af28cd2f021d63261
Ugurhamzaoglu67 <craxx67@gmail.com>
2022-04-16 11:29:04 +0300

    DownloadedVC created with TableView

--- GithubJobs-APP/Cells/DownloadCells/DownloadCollectionCell.swift
+++ GithubJobs-APP/Cells/DownloadCells/DownloadCollectionCell.swift
@@ -7,25 +7,97 @@
 
 import UIKit
 
-class DownloadCollectionCell: UICollectionViewCell {
-    static var identifier =  "DownloadCollectionCell"
+class DownloadTableCell: UITableViewCell {
+    static var identifier =  "DownloadTableCell"
     
-    var imageView: UIImageView = {
+    private let imgCompany: UIImageView = {
         let iv = UIImageView()
-        iv.image = UIImage(named:"v1")
-        iv.clipsToBounds = true
-        iv.layer.cornerRadius = 15
+        iv.image = UIImage(systemName: "house")
         iv.contentMode = .scaleAspectFill
+  
         return iv
     }()
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        contentView.addSubview(imageView)
-        imageView.fillSuperview()
-    }
+    var companyNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Huawei"
+        label.font = .systemFont(ofSize: 21)
+        label.numberOfLines = 0
+        label.font = .boldSystemFont(ofSize: 17)
+        label.textColor = .black
+        
+        return label
+    }()
     
+    
+    private let positionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "iOS Developer"
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 15)
+        label.textColor = .black
+        
+        return label
+    }()
+    
+    private let workTimeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Full Time"
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 15)
+        label.textColor = .black
+        
+        return label
+    }()
+    
+    private let locationLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Boston, MA"
+        label.font = .systemFont(ofSize: 15)
+        label.numberOfLines = 0
+        label.textColor = .black
+      
+        return label
+    }()
+    
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        return stackView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(imgCompany)
+        contentView.addSubview(positionLabel)
+        contentView.addSubview(stackView)
+        [companyNameLabel,positionLabel, locationLabel].forEach { stackView.addArrangedSubview($0)}
+        
+        stackView.alignment = .center
+        stackView.distribution = .fillProportionally
+        setConstraints()
+    }
     required init?(coder: NSCoder) {
         fatalError("not imp")
+    }
+}
+
+
+
+//MARK: -
+extension DownloadTableCell {
+    private func setConstraints(){
+        imgCompany.anchor(top: topAnchor,
+                          leading: leadingAnchor,
+                          bottom: bottomAnchor,
+                          trailing: stackView.leadingAnchor,
+                          padding: .init(top: 5, left:10, bottom: 0, right: 10))
+        
+        stackView.anchor(top: topAnchor,
+                         leading: imgCompany.trailingAnchor,
+                         bottom: bottomAnchor,
+                         trailing: trailingAnchor ,
+                         size: CGSize(width: 200, height: 100)
+        )
     }
 }
--- GithubJobs-APP/Views/DownloadedListVC.swift
+++ GithubJobs-APP/Views/DownloadedListVC.swift
@@ -2,85 +2,53 @@
 //  DownloadedListVC.swift
 //  GithubJobs-APP
 //
-//  Created by ugur-pc on 11.04.2022.
+//  Created by ugur-pc on 16.04.2022.
 //
 
 import UIKit
-import CHTCollectionViewWaterfallLayout
 
-class DownloadedListVC: UIViewController{
+class DownloadedListVC: UIViewController {
+
     
-    func collectionView(_ collectionView: UICollectionView, heightForPhotoAtIndexPath indexPath: IndexPath) -> CGFloat {
+    private let tableView: UITableView = {
+        let tv = UITableView()
+        tv.register(DownloadTableCell.self, forCellReuseIdentifier: DownloadTableCell.identifier)
+        return tv
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    
+    }
+    
+    private func setupViews() {
+        view.addSubview(tableView)
+        title =  "Downloaded List"
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.backgroundColor = .lightGray
+        tableView.fillSuperview()
+    }
+}
+
+extension DownloadedListVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: DownloadTableCell.identifier, for: indexPath) as! DownloadTableCell
+        
+        cell.backgroundColor = .white
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 150
     }
     
     
-     var generalCollectionView: UICollectionView = {
-         let layout = CHTCollectionViewWaterfallLayout()
-         layout.itemRenderDirection = .leftToRight
-         layout.columnCount = 2
-         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        cv.register(DownloadCollectionCell.self,
-                    forCellWithReuseIdentifier: DownloadCollectionCell.identifier)
-        return cv
-    }()
-    struct Model {
-        let imageName: String
-        let height: CGFloat
-    }
-    
-    private var models = [Model]()
-   
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.addSubview(generalCollectionView)
-        
-        let images = Array(1...9).map { "v\($0)"}
-        
-        
-         models = images.compactMap{
-            return Model.init(imageName:$0,
-                              height:CGFloat.random(in: 200...400))
-        }
-        
-        
-        generalCollectionView.fillSuperview()
-        generalCollectionView.backgroundColor = .orange
-        
-        generalCollectionView.delegate = self
-        generalCollectionView.dataSource = self
-        
-        
-    }
-    
 }
-
-extension DownloadedListVC: UICollectionViewDelegate, UICollectionViewDataSource {
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return models.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: DownloadCollectionCell.identifier, for: indexPath) as! DownloadCollectionCell
-        
-        cell.imageView.image =  UIImage(named: "\(self.models[indexPath.row].imageName)")
-       
-        return cell
-    }
-   
-}
-
-
-extension DownloadedListVC: CHTCollectionViewDelegateWaterfallLayout {
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        return CGSize(width: view.frame.size.width / 2,
-                      height: models[indexPath.row].height)
-    }
-    
-    
-}
-
